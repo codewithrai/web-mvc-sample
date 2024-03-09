cb3b2ac788d88f16baacb5a0635eed80d9b9c414
Shafique Anwar <shafianwar349@gmail.com>
2024-03-09 12:45:08 +0500

    Get Mapping Added

--- src/main/java/com/example/controller/TestController.java
+++ src/main/java/com/example/controller/TestController.java
@@ -1,20 +1,23 @@
 package com.example.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping("/")
 public class TestController {
 
-    @RequestMapping(value = "/home", method= RequestMethod.GET)
+
+    @GetMapping("/home")
     public String home() {
         System.out.println("Hello");
         return "index";
     }
 
-    @RequestMapping(value = "/about", method= RequestMethod.GET)
+    @GetMapping("/about")
     public String about() {
         System.out.println("Hello");
         return "about";
